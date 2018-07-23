59c4a76eb735979ddcc2be5ed3a7d1535799cf52
Jayon Kim <jpkim921@gmail.com>
2018-07-23 18:00:22 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180723174608) do
+ActiveRecord::Schema.define(version: 20180723175842) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20180723174608) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
