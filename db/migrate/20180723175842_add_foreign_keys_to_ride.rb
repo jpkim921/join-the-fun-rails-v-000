19e2bc80fd44b31158d07c97442d3c6db3b7aac6
Jayon Kim <jpkim921@gmail.com>
2018-07-23 18:00:05 +0000

    automatically backed up by learn

--- db/migrate/20180723175842_add_foreign_keys_to_ride.rb
+++ db/migrate/20180723175842_add_foreign_keys_to_ride.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToRide < ActiveRecord::Migration
   def change
-    add_column :products, :part_number, :string
-
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
   end
 end
